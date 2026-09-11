82b0bfd8a336c3ded4d670ee52e9625e2a8c336c
Chitwan-bajpai <166581772+Chitwan-bajpai@users.noreply.github.com>
2026-09-11 15:57:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Linked List/reverse-a-linked-list.cpp
@@ -0,0 +1,30 @@
+/* Structure of Linked List Node
+class Node {
+ public:
+    int data ;
+    Node *next ;
+
+    Node(int x) {
+        data = x ;
+        next = nullptr ;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* reverseList(Node* head) {
+        // code here
+        Node *prev=NULL;
+        Node *curr=head;
+        while(curr!=NULL){
+            Node *future=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=future;
+        }
+        head=prev;
+        return head;
+        
+    }
+};
